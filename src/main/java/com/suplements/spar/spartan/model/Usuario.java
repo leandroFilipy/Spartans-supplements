46eb060e8cf63ecc3f8482e7a3ed7d73fece59d7
JOSÉ AZARÍAS PÉREZ TORRES <jose_a_torres@estudante.sesisenai.org.br>
2026-03-18 16:25:17 -0300

    feat: Implementa CRUD para a classe Usuario

--- src/main/java/com/suplements/spar/spartan/model/Usuario.java
+++ src/main/java/com/suplements/spar/spartan/model/Usuario.java
@@ -32,4 +32,13 @@ public class Usuario {
     String cpf;
     LocalDate dataNascimento;
 
+    public Usuario(String nome, String email, String password, String telefone, String endereco, String cpf, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.email = email;
+        this.password = password;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
 }
