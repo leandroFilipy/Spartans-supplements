7781bb2dae77f90b05f85d2df8299e98c2737b5f
LEANDRO FILIPY DE LIMA <leandro_f_lima@estudante.sesisenai.org.br>
2026-03-18 16:11:05 -0300

    feat: adiciona-crud-básico-de-produtos

--- src/main/java/com/suplements/spar/spartan/mapper/ProdutoMapper.java
+++ src/main/java/com/suplements/spar/spartan/mapper/ProdutoMapper.java
@@ -1,4 +1,37 @@
 package com.suplements.spar.spartan.mapper;
 
+import com.suplements.spar.spartan.dto.ProdutoRequest;
+import com.suplements.spar.spartan.dto.ProdutoResponse;
+import com.suplements.spar.spartan.model.Produto;
+import jakarta.persistence.Entity;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProdutoMapper {
+
+    public Produto toEntity(ProdutoRequest produtoRequest){
+
+        return new Produto(produtoRequest.nome(),
+                produtoRequest.preco(),
+                produtoRequest.descricao(),
+                produtoRequest.peso(),
+                produtoRequest.categoria(),
+                produtoRequest.imagemUrl(),
+                produtoRequest.quantidadeEstoque()
+        );
+    }
+
+    public ProdutoResponse toResponse(Produto produto){
+
+        return new ProdutoResponse(
+                produto.getId(),
+                produto.getNome(),
+                produto.getPreco(),
+                produto.getDescricao(),
+                produto.getPeso(),
+                produto.getCategoria(),
+                produto.getImagemUrl(),
+                produto.getQuantidadeEstoque()
+        );
+    }
 }
--- src/main/java/com/suplements/spar/spartan/service/ProdutoService.java
+++ src/main/java/com/suplements/spar/spartan/service/ProdutoService.java
@@ -1,4 +1,72 @@
 package com.suplements.spar.spartan.service;
 
+import com.suplements.spar.spartan.dto.ProdutoRequest;
+import com.suplements.spar.spartan.dto.ProdutoResponse;
+import com.suplements.spar.spartan.mapper.ProdutoMapper;
+import com.suplements.spar.spartan.model.Produto;
+import com.suplements.spar.spartan.repository.ProdutoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+    private final ProdutoMapper produtoMapper;
+
+
+    public ProdutoResponse create(ProdutoRequest produtoRequest){
+
+        Produto produto = produtoMapper.toEntity(produtoRequest);
+        Produto produtoSalvo = produtoRepository.save(produto);
+        ProdutoResponse produtoResponse = produtoMapper.toResponse(produtoSalvo);
+
+        return produtoResponse;
+    }
+
+    public List<ProdutoResponse> list (){
+        List<Produto> produtos = produtoRepository.findAll();
+        List<ProdutoResponse> dtos = new ArrayList<>();
+
+        for (Produto p : produtos){
+            dtos.add(produtoMapper.toResponse(p));
+        }
+
+        return dtos;
+    }
+
+    public ProdutoResponse listById(long id){
+        Produto produto = produtoRepository.findById(id).orElseThrow(() -> new RuntimeException("There is no product with this ID"));
+        ProdutoResponse produtoResponse = produtoMapper.toResponse(produto);
+
+        return produtoResponse;
+    }
+
+    public ProdutoResponse update(long id, ProdutoRequest produtoRequest){
+
+        Produto produto = produtoRepository.findById(id).orElseThrow(() -> new RuntimeException("There is no product with this ID"));
+        produto.setNome(produto.getNome());
+        produto.setPreco(produto.getPreco());
+        produto.setDescricao(produto.getDescricao());
+        produto.setPeso(produto.getPeso());
+        produto.setCategoria(produto.getCategoria());
+        produto.setImagemUrl(produto.getImagemUrl());
+        produto.setQuantidadeEstoque(produto.getQuantidadeEstoque());
+        ProdutoResponse produtoResponse = produtoMapper.toResponse(produto);
+
+        return produtoResponse;
+    }
+
+    public void delete(long id){
+        if(produtoRepository.existsById(id)){
+            produtoRepository.deleteById(id);
+        }else {
+            throw new RuntimeException("There is no product with this ID");
+        }
+    }
+
 }
