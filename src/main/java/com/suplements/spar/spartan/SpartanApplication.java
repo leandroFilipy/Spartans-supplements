703b7faee0710efa3a6aa6c79445c7d7a42b21bf
LEANDRO FILIPY DE LIMA <leandro_f_lima@estudante.sesisenai.org.br>
2026-03-18 14:09:06 -0300

    feat: adiciona estrutura básica

---
+++ src/main/java/com/suplements/spar/spartan/SpartanApplication.java
@@ -0,0 +1,13 @@
+package com.suplements.spar.spartan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suplements/spar/spartan/SpartanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suplements.spar.spartan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
