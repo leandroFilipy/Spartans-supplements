0a1e06daecb6def0b7d8367e12483e05b15192eb
LEANDRO FILIPY DE LIMA <leandro_f_lima@estudante.sesisenai.org.br>
2026-03-25 15:49:52 -0300

    fix: arruma erro grave de endpoit

--- src/main/java/com/suplements/spar/spartan/controller/UsuarioController.java
+++ src/main/java/com/suplements/spar/spartan/controller/UsuarioController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/User")
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UsuarioController {
 
